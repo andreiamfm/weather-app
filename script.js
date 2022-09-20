18c86732726ef768628ed56b6d5d723ca159995c
Andreia Martins <113913596+andreiamfm@users.noreply.github.com>
2022-09-20 12:23:48 +0100

    Update script.js

---
+++ script.js
@@ -0,0 +1,122 @@
+h1,
+h2,
+h5,
+h6,
+h7 {
+  font-family: "Gloria Hallelujah", cursive;
+}
+
+h1 {
+  margin-top: 10x;
+  margin-left: 30px;
+}
+
+h7 {
+  font-size: 14px;
+}
+
+p {
+  font-family: helvetica;
+  font-size: 13px;
+}
+
+img {
+  width: 300px;
+  margin: 20px auto;
+}
+svg {
+  width: 20px;
+}
+
+li {
+  list-style: none;
+  font-family: helvetica;
+  font-size: 13px;
+  padding-left: 0px;
+}
+
+ul {
+  padding-inline-start: 10px;
+}
+
+.form {
+  display: flex;
+  margin-top: 30px;
+}
+
+.form-control {
+  height: 36px;
+  width: 260px;
+  color: #949bbb;
+  border-radius: 0.6em;
+}
+
+.container {
+  background: transparent;
+  display: block;
+  width: fit-content;
+  padding: 40px;
+  margin: 20px auto;
+  border-radius: 20px;
+  box-shadow: 2px 2px 20px 2px rgba(0, 0, 0, 0.1);
+}
+
+.body2 {
+  margin-top: 30px;
+  max-width: 530px;
+  border-radius: 15px;
+  box-shadow: 2px 2px 15px 2px rgba(0, 0, 0, 0.1);
+}
+
+.body2 .row {
+  padding: 20px;
+}
+
+.col {
+  padding-left: 0;
+  margin-top: 20px;
+  position: relative;
+}
+
+.col-sm {
+  position: relative;
+  padding-top: 100px;
+  color: #364170;
+}
+
+.time {
+  margin-top: 40px;
+  margin-bottom: 5px;
+  padding: 10px;
+}
+
+.weather {
+  height: fit-content;
+}
+
+.card {
+  display: block;
+  margin-top: 0px;
+  width: 160px;
+  box-shadow: 2px 2px 15px 2px rgba(0, 0, 0, 0.1);
+}
+
+.btn-primary {
+  height: 38px;
+  border-radius: 10px;
+  margin-top: 8px;
+  background-color: #8791be;
+  border-color: white;
+  color: white;
+  font-size: 16px;
+  line-height: 22px;
+  padding: 8px 15px;
+  text-align: center;
+  text-decoration: none;
+  transition: all 200ms ease;
+}
+
+.btn-primary:hover {
+  color: #fff;
+  background: #364170;
+}
